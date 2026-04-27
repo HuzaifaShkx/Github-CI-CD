1ccbd2ed450e17b408e6083bf9576f3a29c19132
HuzaifaShkx <huzaifayasin225@gmail.com>
2026-04-27 11:14:50 +0500

    sole the redis issue

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-from flask import Flask
-import redis
 import os
+import redis
+from flask import Flask
 
-redis_host = os.getenv('REDIS_HOST', 'localhost')
 app = Flask(__name__)
 
-# Connect to Redis
-redis_host=os.getenv('REDIS_HOST')
-redis_port=int(os.getenv('REDIS_PORT', 6379))
-r = redis.Redis(host=redis_host, port=redis_port)
+redis_host = os.getenv("REDIS_HOST", "localhost")
 
-@app.route('/')
+r = redis.Redis(host=redis_host, port=6379)
+
+@app.route("/")
 def home():
-    count = r.incr('counter')
-    return f'Hello! You visited {count} times 🚀'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/goodbye')
-def goodbye():
-    return 'Goodbye, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        count = r.incr("counter")
+        return f"Visits: {count}"
+    except:
+        return "App running but Redis not connected"
